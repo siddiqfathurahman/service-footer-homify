8f9161fa08562632a9814fef94f3bb089f821aec
Muhammad Siddiq Fathurahman <129701382+siddiqfathurahman@users.noreply.github.com>
2024-09-26 08:45:06 +0700

    Update Voucher.jsx

--- src/components/Voucher.jsx
+++ src/components/Voucher.jsx
@@ -5,7 +5,7 @@ const Cher = () => {
     return (
         <div className="font-poppins flex justify-center items-center mt-24 relative">
             <img className="md:w-24 w-12 absolute top-[-1.5rem] md:left-[11rem] left-[-0.2rem] z-30" src={cctv} alt="cctv" />
-            <img className="md:w-20 w-10 absolute bottom-[-1.5rem] md:right-[12rem] right-[-0.2rem] z-50" src={lampu} alt="lampu" />
+            <img className="md:w-20 w-10 absolute bottom-[-1.5rem] md:right-[12rem] right-[-0.2rem] z-30" src={lampu} alt="lampu" />
             
             <div className="bg-gradient-to-r from-black to-purple-950 via-black bg-black bg-opacity-90 md:py-10 md:px-32 py-3 px-7 rounded-2xl inline-block">
                 <div className="text-center">
